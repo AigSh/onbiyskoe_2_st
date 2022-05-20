43797179e8ebb9e24099c1b0a6d583444cff7a5f
AigSh <aigul.zd@gmail.com>
2022-05-20 11:10:47 +0300

    1_min

--- js/script.js
+++ js/script.js
@@ -4,26 +4,21 @@ $(document).ready(function(){
 		dots:false,
 		slidesToShow:1,
 		autoplay:true,
-		speed:250,
+		speed:500,
 		autoplaySpeed:300,
 		fade: true,
 		waitForAnimate: false,
 		pauseOnHover: false,
+		infinite: false,
+		initialSlide: 0,
 		// asNavFor: ('.slider_2'),
 	}		
 	);
-	$('.slider_2').slick({
-		arrows:true,
-		dots:false,
-		slidesToShow:1,
-		autoplay:true,
-		speed:250,
-		autoplaySpeed:300,
-		fade: true,
-		waitForAnimate: false,
-		pauseOnHover: false,
-		// asNavFor: ('.slider'),
-	}		
-	);
+	
+	$('.slider').on('beforeChange', function(e, slick, current, next){
+		if (next === slick.slideCount-1){
+		   slick.autoPlay = false;
+		}
+	 });
 });
 
